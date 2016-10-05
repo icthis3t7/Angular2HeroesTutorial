0fc57755f86553395d1f459c98c5cce1befc7c9b
icthis3t7 <icthis3t7@gmail.com>
2016-10-04 22:33:08 -0500

    Finished all steps up to the end of Stage 2 https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

---
+++ app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+
+  };
+
+}
+
+
+@Component({
+  selector: 'my-app', /*css selector*/
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+
+  </div>
+  `
+
+})
+
+
+export class Hero {
+  id: number;
+  name: string;
+
+}
