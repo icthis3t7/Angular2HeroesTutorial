0d50ccf46df54988f6c79baf31fddd0cfb1cb283
icthis3t7 <icthis3t7@gmail.com>
2016-10-08 10:11:18 -0500

    Completed Chapter 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 
 @Component({
   selector: 'my-app',
@@ -8,19 +10,14 @@ import { Component } from '@angular/core';
   <h2>My Heroes</h2>
   <ul class="heroes">
     <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
+      [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
+
     </li>
+
   </ul>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
 
   styles: [`
@@ -85,12 +82,6 @@ export class AppComponent {
     this.selectedHero = hero;
 
   }
-
-}
-
-export class Hero {
-  id: number;
-  name: string;
 
 }
 
